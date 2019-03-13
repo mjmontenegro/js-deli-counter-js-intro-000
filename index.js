19a286b60e3bf252bb0d6ba7e177892f9ad93388
Mauricio Javier Montenegro <mjmontenegro@gmail.com>
2019-03-13 23:10:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ function takeANumber(currentLine, name) {
   currentLine.push(name);
   return `Welcome, ${name}. You are number ${currentLine.length} in line.`;
 }
+function nowServing(currentLine) {
+  return currentLine.length ? currentLine.shift() : "There is nobody waiting to be served!"
+}
